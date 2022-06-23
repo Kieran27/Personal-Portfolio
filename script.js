6fcabb3473327360263eddf8ea45d5d0215382fe
Kieran Singh <kieransingh27@gmail.com>
2022-06-23 12:33:36 +1000

    Fixed transparent bg toggle

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const hamburgerBtn = document.querySelector("[data-id='btn-hamburger']")
 const sideNav = document.querySelector("[data-id='sidenav']")
+const body = document.querySelector('body');
 
 hamburgerBtn.addEventListener('click', (e) => {
-  sideNav.classList.toggle('toggle-width')
-  hamburgerBtn.classList.toggle('open')
-  document.querySelector('body').classList.toggle('body-toggle')
-})
+  sideNav.classList.toggle('toggle-width');
+  sideNav.classList.toggle('toggle-transparent-bg');
+  hamburgerBtn.classList.toggle('open');
+  body.classList.toggle('scroll-toggle');
+});
 
 sideNav.querySelectorAll('li').forEach(listItem => {
   listItem.addEventListener('click', (e) => {
-    sideNav.classList.toggle('toggle-width')
-    hamburgerBtn.classList.toggle('open')
-    document.querySelector('body').classList.toggle('body-toggle')
-  })
-})
+    sideNav.classList.toggle('toggle-width');
+    sideNav.classList.toggle('toggle-transparent-bg');
+    hamburgerBtn.classList.toggle('open');
+    body.classList.toggle('scroll-toggle');
+  });
+});
