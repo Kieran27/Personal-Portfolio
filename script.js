43a6d063bc2e7b01c794e6e639d9edaf4d8930a1
Kieran Singh <kieransingh27@gmail.com>
2022-07-26 14:02:23 +1000

    Added scroll animations to projects and about section

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ sideNav.querySelectorAll('li').forEach(listItem => {
     body.classList.toggle('scroll-toggle');
   });
 });
+
+const reveal = () => {
+  const reveals = document.querySelectorAll(".reveal");
+  for (let i=0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("revealed");
+    } else {
+      reveals[i].classList.remove("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
